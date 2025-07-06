6e1b3482d82051b39fddc0dcee0838a7632eefbd
Tallal64 <firefoxtallal@gmail.com>
2025-07-06 11:04:41 +0500

    Add program to reverse an integer in C++

---
+++ binaryNumberSystem/reverseInteger.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+// write a a code to reverse and integer
+{
+    int n = 762, rem;
+    int reverseNum = 0;
+    while (n > 0)
+    {
+        rem = n % 10;
+        n = n / 10;
+        reverseNum = reverseNum * 10 + rem;
+    }
+    cout << reverseNum << endl;
+    return 0;
+}
