735953a781a896a68c7b4b0fe9e7e22c19918eb3
Tallal64 <firefoxtallal@gmail.com>
2025-07-08 10:55:47 +0500

    Add programs to find smallest and largest numbers in an array

---
+++ arrays/indexOfnumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+int main()
+{
+    int size = 6;
+    int arr[] = {12, 5, 15, 0, -15, 24};
+    int smallest = INT_MAX;
+    int index;
+
+    for (int i = 0; i < size; i++)
+    {
+        smallest = min(arr[i], smallest);
+        if (smallest == arr[i])
+        {
+            index = i;
+        }
+    }
+    cout << "smallest number is : " << smallest << endl;
+    cout << "index of smallest number is : " << index << endl;
+    return 0;
+}
