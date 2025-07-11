469a168e4437d8d78d7369b54efd014e6a9be424
Tallal64 <firefoxtallal@gmail.com>
2025-07-11 07:59:36 +0500

    Add program to swap minimum and maximum elements in an array

---
+++ arrays/swapMinAndMax.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+// swap the minimum and maximum in an array
+void swapMinMax(int arr[], int size, int minValue, int maxValue)
+{
+    int maxIndex = size - 1;
+    int minIndex = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == minValue)
+        {
+            minIndex = i;
+        }
+        if (arr[i] == maxValue)
+        {
+            maxIndex = i;
+        }
+    }
+
+    swap(arr[minIndex], arr[maxIndex]);
+}
+
+int main()
+{
+    int arr[] = {20, 3, 0, 5, 12};
+    int size = sizeof(arr) / sizeof(int);
+    int minValue = INT_MAX;
+    int maxValue = INT_MIN;
+
+    cout << "original array....." << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl
+         << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        minValue = min(arr[i], minValue);
+        maxValue = max(arr[i], maxValue);
+    }
+
+    swapMinMax(arr, size, minValue, maxValue);
+
+    cout << "after swaping....." << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
+// TODO: replace microsoft with github in vscode sync
